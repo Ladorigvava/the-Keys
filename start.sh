f75edd2abe19f1b95660185a3c16bfba6c7ed142
Ladorigvava <ladorigvava@gmail.com>
2026-09-11 10:00:10 +0400

    Decode encrypted runtime carrier blocks independently

--- start.sh
+++ start.sh
@@ -5,9 +5,15 @@ set -eu
 EXPECTED_SHA="2cabb1ea98ff4345541eec87c87ca854293f04eb3a1909f42528770fa764fa65"
 
 rm -rf /app/*
-cat /opt/tks/runtime.part1.b64 /opt/tks/runtime.part2.b64 /opt/tks/runtime.part3.b64 /opt/tks/runtime.part4.b64 \
-  | tr -cd 'A-Za-z0-9+/=' \
-  | base64 -d > /tmp/tks-runtime.tar.xz.enc
+: > /tmp/tks-runtime.tar.xz.enc
+for part in \
+  /opt/tks/runtime.part1.b64 \
+  /opt/tks/runtime.part2.b64 \
+  /opt/tks/runtime.part3.b64 \
+  /opt/tks/runtime.part4.b64
+do
+  tr -cd 'A-Za-z0-9+/=' < "$part" | base64 -d >> /tmp/tks-runtime.tar.xz.enc
+done
 
 openssl enc -d -aes-256-cbc -pbkdf2 -iter 200000 \
   -in /tmp/tks-runtime.tar.xz.enc \
